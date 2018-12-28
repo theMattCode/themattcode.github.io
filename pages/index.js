47d8b6a18888b55311264e6e19bc92a6c7e79d56
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2018-12-28 16:08:25 +0100

    Stub index class

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,19 @@
-export default () => <div>Juhu</div>
+import React from "react"
+import Head from "next/head"
+
+class Index extends React.Component {
+	render() {
+		return (
+			<div>
+				<Head>
+					<title>Matthäus Mayer</title>
+					<meta charSet='utf-8'/>
+					<meta name='viewport' content='initial-scale=1.0, width=device-width'/>
+				</Head>
+				<div>Welcome</div>
+			</div>
+		)
+	}
+}
+
+export default Index
