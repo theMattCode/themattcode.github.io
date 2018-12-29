7d60cdd807047f2e6a707cecfb8cbb37c0af18e4
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2018-12-29 21:19:20 +0100

    working links

--- components/Head/Head.jsx
+++ components/Head/Head.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import NextHead from 'next/head'
+import PropTypes from 'prop-types'
 
 class Head extends React.Component {
 	render() {
 		return (
-			<NextHead>
-				<title>{this.props.title}</title>
-				<meta charSet='utf-8'/>
-				<meta name='viewport' content='initial-scale=1.0, width=device-width'/>
-			</NextHead>
+			<div>
+				<style jsx global>{`
+					body {
+						font-family: 'Roboto', sans-serif;
+					}
+				`}</style>
+				<NextHead>
+					<title>{this.props.title}</title>
+					<meta charSet='utf-8'/>
+					<meta name='viewport' content='initial-scale=1.0, width=device-width'/>
+					<link href='https://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet'/>
+				</NextHead>
+			</div>
 		)
 	}
 }
+
+Head.propTypes = {
+	title: PropTypes.string.isRequired
+};
 
 export default Head
