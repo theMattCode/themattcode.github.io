779c1ecafa0854015d9433025267eaea90ca73b9
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2018-12-29 20:53:08 +0100

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e9ae5ca2179150be58ab";
+/******/ 	var hotCurrentHash = "2a49eb11a5954fa64909";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
