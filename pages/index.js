fb15090257f80d81f6516513ecb29950f169e63f
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2018-12-31 22:21:34 +0100

    generalize content directory

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React, {Fragment} from "react";
 import ReactMarkdown from "react-markdown";
 import Head from "../components/Head/Head";
-import About from "../md/index/about.md";
+import About from "../content/index/about.md";
 
 export default () => (
 	<Fragment>
