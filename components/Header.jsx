c3261c6c19d816ad2d3a0c3c9eccdf0cf460163c
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2019-01-02 10:38:24 +0100

    Add header

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const styles = {
+	header: {
+
+	}
+};
+export default () => (
+	<header>
+		Header
+	</header>
+)
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import React, {Fragment} from "react";
 import ReactMarkdown from "react-markdown";
 import Head from "../components/Head";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
 import About from "./index/about.md";
 
 export default () => (
 	<Fragment>
 		<Head title="Matthäus Mayer"/>
+		<Header/>
 		<main>
 			<ReactMarkdown source={About}/>
 		</main>
