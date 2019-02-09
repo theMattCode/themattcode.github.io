07ad7bba041756c4d7507eeff93ff1ea773639fe
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2019-02-09 19:59:10 +0100

    Fix Google Analytics script path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
-const {join} = require('path');
-const {promisify} = require('util');
+const { join } = require('path');
+const { promisify } = require('util');
 const copyFile = promisify(fs.copyFile);
 
 const filesToCopy = new Map();
@@ -8,10 +8,10 @@ filesToCopy.set('build/CNAME', 'CNAME');
 filesToCopy.set('build/.nojekyll', '.nojekyll');
 filesToCopy.set('build/_config.yml', '_config.yml');
 filesToCopy.set('static/icons.css', 'static/icons.css');
-filesToCopy.set('static/gs.js', 'static/ga.js');
+filesToCopy.set('static/ga.js', 'static/ga.js');
 
 module.exports = {
-	exportPathMap: async function (defaultPathMap, {dev, dir, outDir, distDir, buildId}) {
+	exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
 		if (dev) {
 			return defaultPathMap
 		}
